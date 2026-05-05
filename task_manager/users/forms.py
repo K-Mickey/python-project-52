acf90cda8df7ed78ffb1e1bd993166120bd1b038
Mikhail Kokuev <kokmickey@yandex.ru>
2026-05-05 17:00:24 +0500

    add password to update view

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.forms import CharField, ModelForm
+from django.forms.widgets import PasswordInput
 from django.utils.translation import gettext_lazy
 
 from task_manager.users.models import User
@@ -9,7 +10,7 @@ class UserForm(UserCreationForm):
     first_name = CharField(required=True, label=gettext_lazy("First name"))
     last_name = CharField(required=True, label=gettext_lazy("Last name"))
 
-    class Meta(UserCreationForm.Meta):
+    class Meta:
         model = User
         fields = (
             "first_name",
@@ -21,8 +22,17 @@ class UserForm(UserCreationForm):
 
 
 class UserUpdateForm(ModelForm):
-    first_name = CharField(required=True, label=gettext_lazy("First name"))
-    last_name = CharField(required=True, label=gettext_lazy("Last name"))
+    password = CharField(
+        required=False,
+        widget=PasswordInput(),
+        label=gettext_lazy("Password"),
+        help_text=gettext_lazy("Leave blank to keep the same password"),
+    )
+    password_confirm = CharField(
+        required=False,
+        widget=PasswordInput(),
+        label=gettext_lazy("Password confirmation"),
+    )
 
     class Meta:
         model = User
@@ -31,3 +41,21 @@ class UserUpdateForm(ModelForm):
             "last_name",
             "username",
         )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password_confirm = cleaned_data.get("password_confirm")
+        if password and password != password_confirm:
+            error = gettext_lazy("The two password fields didn’t match.")
+            raise self.add_error("password_confirm", error=error)
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        password = self.cleaned_data.get("password")
+        if password:
+            user.set_password(password)
+        if commit:
+            user.save()
+        return user
